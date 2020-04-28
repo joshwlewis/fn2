53f45bee7233d2d5d8ec96de142622eb899a89b4
Josh W Lewis <josh.w.lewis@gmail.com>
2020-04-28 13:12:59 -0500

    simplify secret scrubbing

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ invoke((event, context, logger) => {
   const input = event.data.number;
   const result = input ** 2;
   const fooVal = context.secrets.getValue('foo', 'id');
-  logger.info({ foo: `...${fooVal.substr(fooVal.length - 5).replace(/\s/, '')}`});
+  if (fooVal) {
+    logger.info({ foo: `****${fooVal.substr(fooVal.length - 5)}`});
+  }
   logger.info({ input, result });
   return {result};
 });
